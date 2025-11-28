a8a0fd6d1ca1a1a0267822a788a5843a4e623c69
Harveer <harveersinghnagpal@gmail.com>
2025-11-28 13:52:37 +0530

    aarambh

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-violet-500 text-4xl text-white text-center'>Hello from Dil</h1>
+      <div className="animate-bounce text-7xl">pp</div>
+    </>
+  )
+}
+
+export default App
